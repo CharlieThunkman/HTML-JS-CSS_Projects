222716446eb704e21378b782cbea70caaadc8622
CharlieThunkman <ThunkmanCharlie@gmail.com>
2025-05-25 21:47:39 -0400

    Update extended_js_functions.js
    Added cross-compatibility with videos.html

--- Youtube_Embed_Players/local_site/extended_js_functions.js
+++ Youtube_Embed_Players/local_site/extended_js_functions.js
@@ -3,3 +3,29 @@ function pow(a,b){
 	// n = Math.pow(a,b)
 	return n;
 }
+
+function timeUpdate(){
+	var refresh=100; // Refresh rate in milli seconds
+	mytime=setTimeout('looper()',refresh,lastReadState)
+}
+
+
+function int2bin(i, l=8){
+	return ((i+pow(2,l))%pow(2,l)).toString(2).padStart(l,"0");
+}
+
+function timeFormat(f,h=true){
+	let hour = Math.floor(f/60/60);
+	let minute = Math.floor(f/60)%60;
+	let second = Math.floor(f)%60;
+	if(h){
+		return hour + ":" + minute.toString().padStart(2,"0") + ":" + second.toString().padStart(2,"0");
+	} else {
+		minute += hour*60;
+		return minute.toString().padStart(1,"0") + ":" + second.toString().padStart(2,"0");
+		}
+}
+
+function isEqual(arr1, arr2){
+	return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
